b8bbb6b4f00bcf5527b7265ccdd3571b2a96fa5e
_____Dy_____ <61171836+Mr-Dy986@users.noreply.github.com>
2022-10-09 04:22:21 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 
-function HomePage() {
+function HomePage () {
   return (
     <MainLayout>
       <div className="bg-light p-5 mt-4 rounded-3">
-        <h1 style={{ color: 'darkred' }}>ផ្លូវ-The Street</h1>
+        <h1 style={ { color: 'dark' } }>ផ្លូវ-The Street</h1>
         <p></p>
         <p>
-          សូមស្វាគមន៍មកកាន់កម្មវិធីកម្មង់ផ្ទាល់ពីអតិថិជន។
-          លោកអ្នកអាចទំនាក់ទំនងសម្រាប់ព័ត៌មានបន្ថែមបានតាមរយៈ៖
+          សូមស្វាគមន៍មកកាន់កម្មវិធីកម្មង់ដោយផ្ទាល់ពីអតិថិជន។
+          លោកអ្នកអាចទំនាក់ទំនងសម្រាប់ព័ត៌មានបន្ថែមបានតាមរយៈ ៖
         </p>
         <p>លេខទូរស័ព្ទ 086679666, 078500203, 010526642</p>
         <p>Telegram,FaceBook Page: ផ្លូវ-The Street</p>

5f04a1c2bd97b801b8b4f8a2e99df0bcb214118e
_____Dy_____ <khoeunmarady@gmail.com>
2022-10-24 17:50:01 +0700

    create 6 files and update 1 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import POSPage from './pages/POSPage';
 
